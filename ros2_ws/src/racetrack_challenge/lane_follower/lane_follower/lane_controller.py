29eece4609b647d3c48d3f7a8d9b5d50706b8472
Sunny Deshpande <your_email@example.com>
2025-04-21 18:41:08 +0800

    modified lane following nodes

--- ros2_ws/src/racetrack_challenge/lane_follower/lane_follower/lane_controller.py
+++ ros2_ws/src/racetrack_challenge/lane_follower/lane_follower/lane_controller.py
@@ -16,8 +16,9 @@ class LaneDetector(Node):
         self.get_logger().info('Lane controller node started')
         self.image_size_x = 320.0
         self.image_size_y = 240.0
-        self.dist_scale_factor = 1.0
-        self.steer_scale_factor = 0.1
+        self.dist_scale_factor = 0.5
+        self.steer_scale_factor = 0.03
+        self.fwd_speed = 0.55
 
         self.create_subscription(
             Point, 
@@ -27,7 +28,7 @@ class LaneDetector(Node):
         )
 
         # Numeric publishers
-        self.cmd_vel_pub = self.create_publisher(Twist, '/temp/cmd_vel', QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))
+        self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))
 
     def lane_centroid_callback(self, msg: Point):
         centroid_x = msg.x
@@ -37,7 +38,7 @@ class LaneDetector(Node):
         img_centre_y = self.image_size_y/2
 
         cmd_vel_msg = Twist()
-        cmd_vel_msg.linear.x = 1.0
+        cmd_vel_msg.linear.x = self.fwd_speed
         cmd_vel_msg.linear.y = 0.0
         cmd_vel_msg.linear.z = 0.0
 
@@ -45,9 +46,10 @@ class LaneDetector(Node):
         cmd_vel_msg.angular.y = 0.0
 
         err_x = centroid_x - img_centre_x
+        # print(err_x)
         dist_from_robot = 1.0 / (1.0 - ((max(img_centre_y, centroid_y) - img_centre_y)/img_centre_y) + 0.05) # 0.05 to avoid divide-by-zero
-        dist_scale = max(1.0, self.dist_scale_factor*dist_from_robot)
-        steer_val = dist_scale * self.steer_scale_factor * err_x
+        dist_scale = min(1.0, self.dist_scale_factor*dist_from_robot)
+        steer_val = -dist_scale * self.steer_scale_factor * err_x
 
         cmd_vel_msg.angular.z = steer_val
         self.cmd_vel_pub.publish(cmd_vel_msg)
